b0315a56e2f2eca10fccfc63832b83a5e07809fc
nene-k27 <e.kobaladze@yahoo.com>
2024-12-29 22:50:52 +0400

    change varassign to arithm

--- src/VarAssign.java
+++ src/VarAssign.java
@@ -1,11 +1,11 @@
 import java.util.Map;
 
 public class VarAssign {
-    public void handleAssignment(String line, Map<String, Integer> vars, VarAssign varAssign) {
+    public void handleAssignment(String line, Map<String, Integer> vars, Arithm arithm) {
         String[] parts = line.split("=");
         String varName = parts[0].trim();
         String expr = parts[1].trim();
-        int value = varAssign.calc(expr, vars);
+        int value = arithm.calc(expr, vars);
         vars.put(varName, value);
     }
 }
